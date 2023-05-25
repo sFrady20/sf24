fc8cbcf1ee0fccff62792d648b748ca871978341
Steven Frady <sfrady20@gmail.com>
2023-05-25 11:14:46 -0400

    coil

--- src/shaders/genuary/2022/8.frag.glsl
+++ src/shaders/genuary/2022/8.frag.glsl
@@ -20,19 +20,43 @@ const vec3[paletteSize]palette=vec3[](
   vec3(11.,24.,37.)
 );
 
-float sdBox(in vec2 p,in vec2 b){
-  vec2 d=abs(p)-b;
-  return length(max(d,0.))+min(max(d.x,d.y),0.);
+//https://www.shadertoy.com/view/DlSXDd
+#define PI 3.14159265
+#define RHO 1.5707963268
+float atan2(in vec2 p,in float w){// :)
+  float a=abs(p.x)<1e-8?RHO:atan(abs(p.y/p.x));
+  float sy=2.*smoothstep(-w,w,p.y)-1.;
+  return abs(a+PI*min(0.,sign(p.x)))*sy;
 }
 
-float sdCircle(in vec2 p,in float r){
-  return length(p);
+#define COIL_LENGTH 10
+#define COIL_HEIGHT.25
+float sdCoil(in vec2 p,in vec2 c,in float r){
+  float d=1.;
+  for(int i=0;i<COIL_LENGTH;i++){
+    float h=COIL_HEIGHT*float(i);
+    vec2 py=p-vec2(0.,h);
+    vec2 cy=c-vec2(0.,h);
+    vec2 yp=cy-py;
+    float a=atan2(yp,1./resolution.y);
+    //return a;
+    py+=vec2(0.,a*COIL_HEIGHT);
+    d=min(d,abs(r-length(py)));
+  }
+  return d;
 }
 
 void coil(in vec2 uv,inout vec3 col){
   uv-=vec2(.5);
   uv*=vec2(resolution.x/resolution.y,1.);
-  col=vec3(step(abs(.5-sdCircle(uv,.5)),.01));
+  
+  //"tilt"
+  uv*=vec2(1.,1.5);
+  
+  //float p=step(sdCoil(uv,vec2(.5,.7),.5),.01);
+  float p=sdCoil(uv,vec2(uv),.5);
+  
+  col=vec3(p);
 }
 
 void main(){
--- src/shaders/genuary/2022/9.frag.glsl
+++ src/shaders/genuary/2022/9.frag.glsl
@@ -7,6 +7,26 @@ uniform sampler2D scene;
 uniform float enter;
 uniform float exit;
 
+float getDist(vec3 p){
+  return 1.;
+}
+
+#define MAX_STEPS 10
+#define MAX_DIST 100.
+#define SURFACE_DIST.1
+float march(vec3 ro,vec3 rd){
+  vec3 p=ro;
+  float d=0.;
+  float ds=0.;
+  for(int i=0;i<MAX_STEPS;i++){
+    p+=ro+d*rd;
+    ds=getDist(p);
+    d+=ds;
+    if(ds<SURFACE_DIST||d>MAX_DIST)break;
+  }
+  return d;
+}
+
 void main(){
   vec2 uv=gl_FragCoord.xy/resolution.xy;
   vec4 color=vec4(uv,sin(time),1.);
